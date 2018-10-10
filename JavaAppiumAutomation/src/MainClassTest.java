fa9a8a708d0f274988e6be59acea6223c57235dc
KamaCarnival <eykamyshnikov@gmail.com>
2018-10-11 02:21:02 +0300

    Homework_test2

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -6,6 +6,12 @@ public class MainClassTest extends MainClass
     @Test
     public void testGetLocalNumber()
     {
-        Assert.assertTrue("Метод вернул значение отличное от 14",getLocalNumber()==14);
+        Assert.assertTrue("Метод getLocalNumber вернул значение отличное от 14",getLocalNumber()==14);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("Метод getClass_number вернул значение меньше 45",getClass_number() > 45);
     }
 }
