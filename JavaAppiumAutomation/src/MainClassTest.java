b689fdf53ff4973cee318099ff33e55c94fb0573
KamaCarnival <eykamyshnikov@gmail.com>
2018-10-11 02:38:30 +0300

    Homework_test3

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -14,4 +14,11 @@ public class MainClassTest extends MainClass
     {
         Assert.assertTrue("Метод getClass_number вернул значение меньше 45",getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString()
+    {
+        Assert.assertTrue("Метод getClassString не вернул строку содержащую \"hello\" или \"hello\"",
+                            getClassString().contains("hello")||getClassString().contains("Hello"));
+    }
 }
